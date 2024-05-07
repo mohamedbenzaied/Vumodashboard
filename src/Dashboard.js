f96975541bea6f076b618ceefdcb85ad490a90fc
mohamedbenzaied <benzaiedmohamed0@gmail.com>
2024-05-07 21:48:23 +0100

    improve

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,7 +10,7 @@ const TableauDashboard = ({ token }) => {
     <div>
       {/* Container for embedding the Tableau dashboard */}
       <tableau-viz id="tableauViz"
-        src={process.env.uri}
+        src='https://prod-uk-a.online.tableau.com/t/slawekpotasz701cda0c89/views/holman-shared/Enterpriseclean'
         token={token}
         toolbar="hidden" hide-tabs>
       </tableau-viz>
