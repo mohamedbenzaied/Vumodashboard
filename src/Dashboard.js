7cd40a75830877d6d4f422e53d79ea62eb1771ec
mohamedbenzaied <77299109+mohamedbenzaied@users.noreply.github.com>
2024-05-29 16:44:04 +0000

    tst

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,39 +1,59 @@
-import React, { useEffect,useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const TableauDashboard = ({ }) => {
-    const [token, setToken] = useState('');
+const TableauDashboard = () => {
+  const [token, setToken] = useState('');
+  const vizRef = useRef(null);
 
-    useEffect(() => {
-      const fetchToken = async () => {
-        try {
-          const response = await fetch('https://backauth.azurewebsites.net/gettoken');
-          if (!response.ok) {
-            throw new Error('Failed to fetch token');
-          }
-          const data = await response.json();
-          console.error('data fetching token:', data);
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const response = await fetch('https://backauth.azurewebsites.net/gettoken');
+        if (!response.ok) {
+          throw new Error('Failed to fetch token');
+        }
+        const data = await response.json();
+        console.log('Fetched token:', data);
 
-          setToken(data.token);
-        } catch (error) {
-          console.error('Error fetching token:', error);
+        setToken(data.token);
+      } catch (error) {
+        console.error('Error fetching token:', error);
+      }
+    };
+
+    fetchToken();
+  }, []);
+
+  useEffect(() => {
+    if (token && vizRef.current) {
+      const { tableau } = window;
+      const vizUrl = 'https://prod-uk-a.online.tableau.com/t/slawekpotasz701cda0c89/views/vumobiv0_1/Overview';
+      const options = {
+        hideTabs: true,
+        toolbar: 'hidden',
+        height: '800px',
+        width: '100%',
+        token: token,
+        onFirstInteractive: () => {
+          const viz = vizRef.current.viz;
+          const workbook = viz.getWorkbook();
+          const activeSheet = workbook.getActiveSheet();
+
+          // Apply filters
+          activeSheet.applyFilterAsync('VIN', 'ZFFLG40A8R0097259', tableau.FilterUpdateType.REPLACE)
+            .then(() => {
+              console.log('Filter applied successfully');
+            })
+            .catch(err => {
+              console.error('Error applying filter:', err);
+            });
         }
       };
-  
-      fetchToken();
-    }, []);
-  return (
-    <div>
-      {/* Container for embedding the Tableau dashboard */}
-      <tableau-viz id="tableauViz"
-        src='https://prod-uk-a.online.tableau.com/t/slawekpotasz701cda0c89/views/vumobiv0_1/Overview'
-        token={token}
-        toolbar="hidden" hide-tabs>
-          <viz-filter field="VIN" value="ZFFLG40A8R0097259"> </viz-filter> 
-          
-      </tableau-viz>
-      
-    </div>
-  );
+
+      vizRef.current.viz = new tableau.Viz(vizRef.current, vizUrl, options);
+    }
+  }, [token]);
+
+  return <div ref={vizRef}></div>;
 };
 
 export default TableauDashboard;
